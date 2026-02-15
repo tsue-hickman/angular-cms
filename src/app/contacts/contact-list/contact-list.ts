47b5f0879dcc2b135b4c6c299be4e7df4efec05e
tsue-hickman <student@example.com>
2026-02-15 14:00:00 +0000

    Build contact-list component
    Created the contact list with dummy data for testing.
    Used *ngFor to loop through contacts.
    Added Bootstrap styling for the panel.

---
+++ src/app/contacts/contact-list/contact-list.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Contact } from '../contact.model';
+import { CommonModule } from '@angular/common';
+import { ContactItemComponent } from '../contact-item/contact-item';
+
+@Component({
+  selector: 'cms-contact-list',
+  imports: [CommonModule, ContactItemComponent],
+  templateUrl: './contact-list.html',
+  styleUrl: './contact-list.css',
+})
+export class ContactListComponent {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+  
+  contacts: Contact[] = [
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null
+    )
+  ];
+
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
+}
