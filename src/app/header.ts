bdea30f8d261782c091c967217e271ba99d6646f
tsue-hickman <student@example.com>
2026-02-15 10:15:00 +0000

    Create header component
    Built the header with WeLearn CMS branding.
    Added navigation links for Documents, Messages, and Contacts.
    Included User dropdown menu.

---
+++ src/app/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class HeaderComponent {
+
+}
