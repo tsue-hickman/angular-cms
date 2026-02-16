63491fb26f115cd0374db6c021e51cb5efed7e36
tsue-hickman <student@example.com>
2026-02-16 14:30:00 +0000

    Create document components structure
    Built documents, document-list, document-item, document-detail.
    Basic structure in place for Week 4.

---
+++ src/app/documents/document-item/document-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-document-item',
+  imports: [],
+  templateUrl: './document-item.html',
+  styleUrl: './document-item.css',
+})
+export class DocumentItem {
+
+}
---
+++ src/app/documents/document-list/document-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-document-list',
+  imports: [],
+  templateUrl: './document-list.html',
+  styleUrl: './document-list.css',
+})
+export class DocumentList {
+
+}
