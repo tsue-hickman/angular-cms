25aa2700e028f6861d98ffb70c22a21c4572e238
tsue-hickman <student@example.com>
2026-02-16 09:00:00 +0000

    Create contact-item component for modularity
    Started making the code more modular.
    ContactItem will display individual contacts in the list.

---
+++ src/app/contacts/contact-item/contact-item.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-item',
+  imports: [],
+  templateUrl: './contact-item.html',
+  styleUrl: './contact-item.css',
+})
+export class ContactItemComponent {
+  @Input() contact!: Contact;
+}
