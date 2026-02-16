fba16617c14444cf4223af2c2edb8dfd38d68d4f
tsue-hickman <student@example.com>
2026-02-16 17:30:00 +0000

    Build message components
    Created message-list, message-item, message-edit components.
    Messages feature taking shape!

---
+++ src/app/messages/message-edit/message-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-edit',
+  imports: [],
+  templateUrl: './message-edit.html',
+  styleUrl: './message-edit.css',
+})
+export class MessageEdit {
+
+}
---
+++ src/app/messages/message-list/message-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-list',
+  imports: [],
+  templateUrl: './message-list.html',
+  styleUrl: './message-list.css',
+})
+export class MessageList {
+
+}
