bb9602f15fcfdbcad8bba830f2d941e24a5a6efd
tsue-hickman <student@example.com>
2026-02-17 15:25:01 -0600

    Implement MessageItem component
    Added @Input decorator to receive message object.
    Displaying sender name and message text.
    Applied CSS styling for message display.

--- src/app/messages/message-item/message-item.ts
+++ src/app/messages/message-item/message-item.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-item',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './message-item.html',
   styleUrl: './message-item.css',
 })
-export class MessageItem {
-
+export class MessageItemComponent {
+  @Input() message!: Message;
 }
