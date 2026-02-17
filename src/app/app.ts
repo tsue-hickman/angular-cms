6f90123a27e1c36bad2db8e692122d857e2ddb80
tsue-hickman <student@example.com>
2026-02-17 16:25:07 -0600

    Implement view switching in app component
    Added switchView method.
    Using ngIf to conditionally display components.
    Imported Documents and MessageList components.
    Week 3 complete - all features working! Fix documents component structure as well.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header';
 import { ContactsComponent } from './contacts/contacts';
+import { MessageListComponent } from './messages/message-list/message-list';
+import { DocumentsComponent } from './documents/documents';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cms-root',
-  imports: [HeaderComponent, ContactsComponent],
+  imports: [CommonModule, HeaderComponent, ContactsComponent, MessageListComponent, DocumentsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
   title = 'cms';
+  selectedFeature = 'contacts';
+
+  switchView(feature: string) {
+    this.selectedFeature = feature;
+  }
 }
