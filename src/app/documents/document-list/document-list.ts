7e3f375be1d0d7a2b03eb2652bdaf2daeaf8050d
tsue-hickman <student@example.com>
2026-02-18 09:34:03 -0600

    Complete DocumentList component
    Added dummy documents for testing.
    Implemented selectedDocumentEvent emitter.
    Using ngFor to display document items.
    Click handler to emit selected document.

--- src/app/documents/document-list/document-list.ts
+++ src/app/documents/document-list/document-list.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
+import { DocumentItemComponent } from '../document-item/document-item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cms-document-list',
-  imports: [],
+  imports: [CommonModule, DocumentItemComponent],
   templateUrl: './document-list.html',
   styleUrl: './document-list.css',
 })
-export class DocumentList {
+export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document('1', 'CIT 425 - Data Warehousing', 'Study guide for final exam', 'https://rkjdatawarehousing.com', null),
+    new Document('2', 'CIT 460 - Network Security', 'Network security final project', 'https://rkjnetsecurity.com', null),
+    new Document('3', 'CIT 366 - Full Stack Development', 'Angular tutorial', 'https://angular.io/tutorial', null),
+    new Document('4', 'CIT 240 - Intro to Networking', 'Favorite link article on Networking', 'https://www.cisco.com', null),
+    new Document('5', 'CIT 370 - Computer Architecture', 'Quantum computing article', 'https://www.ibm.com/quantum', null)
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
