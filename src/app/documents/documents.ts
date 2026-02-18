51d115b382dd40833967331d2a8aa6c171032a6e
tsue-hickman <student@example.com>
2026-02-18 10:14:02 -0600

    Wire up Documents parent component
    Listening for selectedDocumentEvent from list.
    Passing selected document to detail component.
    Using ngIf to conditionally display detail.

--- src/app/documents/documents.ts
+++ src/app/documents/documents.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Document } from './document.model';
+import { DocumentListComponent } from './document-list/document-list';
+import { DocumentDetailComponent } from './document-detail/document-detail';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cms-documents',
-  imports: [],
+  imports: [CommonModule, DocumentListComponent, DocumentDetailComponent],
   templateUrl: './documents.html',
   styleUrl: './documents.css',
 })
 export class DocumentsComponent {
-
+  selectedDocument!: Document;
 }
