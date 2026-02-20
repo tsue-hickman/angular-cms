f62c838a84448bc42b6e35a9b34840a53e5fe7ba
tsue-hickman <student@example.com>
2026-02-20 15:15:32 -0600

    Add document child routes and DocumentEdit component
    Created DocumentEdit component for adding and editing documents.
    Added child routes for documents: new, :id, and :id/edit.
    Replaced document-detail tag with router-outlet in documents component.
    Document routing structure in place.

--- src/app/documents/documents.ts
+++ src/app/documents/documents.ts
@@ -1,26 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Document } from './document.model';
+import { Component } from '@angular/core';
 import { DocumentListComponent } from './document-list/document-list';
-import { DocumentDetailComponent } from './document-detail/document-detail';
-import { CommonModule } from '@angular/common';
-import { DocumentService } from './document.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cms-documents',
-  imports: [CommonModule, DocumentListComponent, DocumentDetailComponent],
+  imports: [DocumentListComponent, RouterModule],
   templateUrl: './documents.html',
   styleUrl: './documents.css',
 })
-export class DocumentsComponent implements OnInit {
-  selectedDocument!: Document;
-
-  constructor(private documentService: DocumentService) {}
-
-  ngOnInit() {
-    this.documentService.documentSelectedEvent.subscribe(
-      (document: Document) => {
-        this.selectedDocument = document;
-      }
-    );
-  }
-}
+export class DocumentsComponent {}
