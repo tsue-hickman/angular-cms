a7e744a93339a4ab5eb05f5e95aa39735bb7ebd2
tsue-hickman <student@example.com>
2026-02-20 17:31:29 -0600

    Add contact child routes and ContactEdit component
    Created ContactEdit component for adding and editing contacts.
    Added child routes for contacts: new, :id, and :id/edit.
    Replaced contact-detail tag with router-outlet in contacts component.
    Contact routing structure in place.

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -1,26 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Contact } from './contact.model';
+import { Component } from '@angular/core';
 import { ContactListComponent } from './contact-list/contact-list';
-import { ContactDetailComponent } from './contact-detail/contact-detail';
-import { CommonModule } from '@angular/common';
-import { ContactService } from './contact.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cms-contacts',
-  imports: [CommonModule, ContactListComponent, ContactDetailComponent],
+  imports: [ContactListComponent, RouterModule],
   templateUrl: './contacts.html',
   styleUrl: './contacts.css',
 })
-export class ContactsComponent implements OnInit {
-  selectedContact!: Contact;
-
-  constructor(private contactService: ContactService) {}
-
-  ngOnInit() {
-    this.contactService.contactSelectedEvent.subscribe(
-      (contact: Contact) => {
-        this.selectedContact = contact;
-      }
-    );
-  }
-}
+export class ContactsComponent {}
