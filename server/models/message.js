b5d3855ee85d5952f68dd22439deae036bdc363d
tsue-hickman <student@example.com>
2026-03-26 07:37:49 -0500

    Week 11: Fix message sender to reference Contact ObjectId

--- server/models/message.js
+++ server/models/message.js
@@ -4,7 +4,7 @@ const messageSchema = mongoose.Schema({
   id: { type: String, required: true },
   subject: { type: String },
   msgText: { type: String, required: true },
-  sender: { type: String }
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
