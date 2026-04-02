87fe4ac93a9eff4652752b061cf957bb5f5fafa9
tsue-hickman <student@example.com>
2026-04-01 20:02:31 -0500

    Week 11: Update sequenceGenerator to use promise-based Mongoose syntax

--- server/routes/sequenceGenerator.js
+++ server/routes/sequenceGenerator.js
@@ -6,54 +6,46 @@ var maxContactId;
 var sequenceId = null;
 
 function SequenceGenerator() {
-
   Sequence.findOne()
-    .exec(function(err, sequence) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An error occurred',
-          error: err
-        });
-      }
-
+    .then(sequence => {
       sequenceId = sequence._id;
       maxDocumentId = sequence.maxDocumentId;
       maxMessageId = sequence.maxMessageId;
       maxContactId = sequence.maxContactId;
+    })
+    .catch(err => {
+      console.log('SequenceGenerator error: ' + err);
     });
 }
 
 SequenceGenerator.prototype.nextId = function(collectionType) {
-
   var updateObject = {};
   var nextId;
 
   switch (collectionType) {
     case 'documents':
       maxDocumentId++;
-      updateObject = {maxDocumentId: maxDocumentId};
+      updateObject = { maxDocumentId: maxDocumentId };
       nextId = maxDocumentId;
       break;
     case 'messages':
       maxMessageId++;
-      updateObject = {maxMessageId: maxMessageId};
+      updateObject = { maxMessageId: maxMessageId };
       nextId = maxMessageId;
       break;
     case 'contacts':
       maxContactId++;
-      updateObject = {maxContactId: maxContactId};
+      updateObject = { maxContactId: maxContactId };
       nextId = maxContactId;
       break;
     default:
       return -1;
   }
 
-  Sequence.update({_id: sequenceId}, {$set: updateObject},
-    function(err) {
-      if (err) {
-        console.log("nextId error = " + err);
-        return null
-      }
+  Sequence.updateOne({ _id: sequenceId }, { $set: updateObject })
+    .then(() => {})
+    .catch(err => {
+      console.log('nextId error = ' + err);
     });
 
   return nextId;
